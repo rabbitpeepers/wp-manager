24cbde8304bc520719c945610701517d3add34f4
Mykola Vasylenko <mykola.vasylenko@zaelab.com>
2020-05-13 16:48:39 +0300

    Make logo clickable

--- web/src/components/AppHeader/Logo.tsx
+++ web/src/components/AppHeader/Logo.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
 import { Box, Button, Text } from 'grommet'
+
 import { Cli } from 'grommet-icons'
+import { Link } from '@reach/router'
+import React from 'react'
 import { useTranslation } from 'react-i18next'
 
 export const Logo: React.FC = () => {
   const { t } = useTranslation()
 
   return (
-    <Box direction="row">
-      <Button icon={<Cli />} hoverIndicator />
-      <Text alignSelf="center" weight="bold">
-        {t('app.name')}
-      </Text>
-    </Box>
+    <Link color="white" to="/" style={{ textDecoration: 'none' }}>
+      <Box direction="row">
+        <Button icon={<Cli />} hoverIndicator />
+        <Text alignSelf="center" weight="bold" color="white">
+          {t('app.name')}
+        </Text>
+      </Box>
+    </Link>
   )
 }
