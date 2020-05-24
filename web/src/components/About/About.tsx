d019893f2cf98f52f081244ae02c5e94a0ed0e00
Mykola Vasylenko <mykola.vasylenko@zaelab.com>
2020-05-24 19:19:24 +0300

    Add version display

--- web/src/components/About/About.tsx
+++ web/src/components/About/About.tsx
@@ -1,8 +1,9 @@
-import { Box, Heading } from 'grommet'
+import { Box, Heading, Text } from 'grommet'
 
 import { Page } from 'src/components/Page/Page'
 import React from 'react'
 import { RouteComponentProps } from '@reach/router'
+import { Version } from 'src/components/About/Version'
 import { useTranslation } from 'react-i18next'
 
 export const About: React.FC<RouteComponentProps> = () => {
@@ -14,7 +15,12 @@ export const About: React.FC<RouteComponentProps> = () => {
         {t('about.title')}
       </Heading>
       <Box>
-        <p>WPManager - a tool for managing Wordpress instances via HELM operator.</p>
+        <Text
+          margin={{ bottom: 'small' }}
+        >
+          WPManager - a tool for managing Wordpress instances via HELM operator.
+        </Text>
+        <Version />
       </Box>
     </Page>
   )
